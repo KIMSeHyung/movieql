9d71b415618232ccff06286a98c5b199ff8e00b3
KIM <tpgud33@gmail.com>
2020-03-11 11:41:03 +0900

    Mutation까지함

---
+++ graphql/db.js
@@ -0,0 +1,43 @@
+export let movies = [{
+        id: 0,
+        name: "Avengers - The new one",
+        score: 8
+    },
+    {
+        id: 1,
+        name: "The Godfather I",
+        score: 99
+    },
+    {
+        id: 2,
+        name: "Logan",
+        score: 2
+    }
+];
+
+export const getMovies = () => movies;
+
+export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+}
